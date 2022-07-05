6520ecef7cb8c6eca26b522e67348f11816dc20f
kitamuraDev <masato1999824@icloud.com>
2022-07-05 21:41:20 +0900

    :sparkles: CRUD機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,124 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 
-const App: FC = () => (
-  <div>
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input type='text' value='' onChange={(e) => e.preventDefault()} />
-      <input type='submit' value='追加' onSubmit={(e) => e.preventDefault()} />
-    </form>
-  </div>
-);
+type Todo = {
+  value: string;
+  readonly id: number;
+  complated: boolean;
+  removed: boolean;
+};
+
+const App: FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const input = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    input.current?.focus();
+  }, []);
+
+  const handleOnSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      // 空文字 || スペースを含む空文字をsubmitさせない
+      if (!input.current?.value.trim()) {
+        input.current!.value = '';
+
+        return;
+      }
+
+      setTodos([
+        {
+          value: input.current.value,
+          id: new Date().getTime(),
+          complated: false,
+          removed: false,
+        },
+        ...todos,
+      ]);
+      input.current.value = '';
+    },
+    [todos],
+  );
+
+  const handleOnEdit = useCallback(
+    (id: Todo['id'], value: Todo['value']) => {
+      const newTodos = todos.map((todo) => {
+        const copyOfTodo = todo;
+        if (todo.id === id) copyOfTodo.value = value;
+
+        return copyOfTodo;
+      });
+
+      setTodos(newTodos);
+    },
+    [todos],
+  );
+
+  const handleOnCheck = useCallback(
+    (id: Todo['id'], complated: Todo['complated']) => {
+      const newTodos = todos.map((todo) => {
+        const copyOfTodo = todo;
+        if (todo.id === id) copyOfTodo.complated = !complated;
+
+        return copyOfTodo;
+      });
+
+      setTodos(newTodos);
+    },
+    [todos],
+  );
+
+  const handleOnRemove = useCallback(
+    (id: Todo['id'], removed: Todo['removed']) => {
+      const newTodos = todos.map((todo) => {
+        const copyOfTodo = todo;
+        if (todo.id === id) copyOfTodo.removed = !removed;
+
+        return copyOfTodo;
+      });
+
+      setTodos(newTodos);
+    },
+    [todos],
+  );
+
+  return (
+    <div>
+      <form onSubmit={(e) => handleOnSubmit(e)}>
+        <input type='text' ref={input} />
+        <input
+          type='submit'
+          value='追加'
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type='checkbox'
+              checked={todo.complated}
+              disabled={todo.removed}
+              onChange={() => handleOnCheck(todo.id, todo.complated)}
+            />
+            <input
+              type='text'
+              value={todo.value}
+              disabled={todo.complated || todo.removed}
+              onChange={(e) => handleOnEdit(todo.id, e.target.value)}
+            />
+            <button
+              type='button'
+              onClick={() => handleOnRemove(todo.id, todo.removed)}
+            >
+              {todo.removed ? '復元' : '削除'}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
