9dc688591fc9ec04663a8da6bcf8d287ac83a20c
kitamuraDev <masato1999824@icloud.com>
2022-07-18 22:26:11 +0900

    :lipstick: フォントをCDN経由で読み込むように設定

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import { indigo, pink } from '@mui/material/colors';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
